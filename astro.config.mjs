533130ed196445b260f085093ea9340346a0127e
Karthik G. Appiah <kar@kar-m1pro.local>
2026-09-21 17:14:51 -0700

    build: init tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // DOCS: https://astro.build/config
 
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
